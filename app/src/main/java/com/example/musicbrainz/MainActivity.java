90b2bdb82cf6b274e2f1bab4720b1e14c9810d1a
YazooKazoo <30350331+YazooKazoo@users.noreply.github.com>
2021-01-03 19:06:20 +0700

    Added main fragment + edited manifest & main activity

--- app/src/main/java/com/example/musicbrainz/MainActivity.java
+++ app/src/main/java/com/example/musicbrainz/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.musicbrainz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager;
+    private MainFragment mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.mainFragment = new MainFragment();
+        FragmentTransaction ft = this.fragmentManager.beginTransaction();
+        ft.add(R.id.fragment_container, this.mainFragment).addToBackStack(null).commit();
     }
 }
---
+++ app/src/main/java/com/example/musicbrainz/MainFragment.java
@@ -0,0 +1,19 @@
+package com.example.musicbrainz;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
